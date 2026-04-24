19a1a7c0fc3b70e483e12e8e94546bcd30f646f5
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-24 10:33:19 +0200

    Add 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
-    
+
     if operator == "+":
         result = add(a, b)
     elif operator == "-":
@@ -22,5 +22,5 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     print("{} {} {} = {}".format(a, operator, b, result))
