7b08444cd4901f788e8eea9d2a73f0ddc1662762
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-28 17:05:23 +0200

    intersection of teo sets

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,3 +4,13 @@ def uniq_add(my_list=[]):
     unique_integers = set(my_list)
     total = sum(unique_integers)
     return total
+def uniq_add1(my_list=[]):
+    """Adds all unique integers in a list (only once for each integer)."""
+    seen = []
+    total = 0
+    for value in my_list:
+        if value not in seen:
+            seen.append(value)
+            total += value
+    return total
+        seen.append(value)
