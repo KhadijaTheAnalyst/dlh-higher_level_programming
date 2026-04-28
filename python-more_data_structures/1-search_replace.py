bf4bc6e548a11aa390a61dc0ae58cacbdbf102c2
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-28 16:28:25 +0200

    replacing a value in the list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
             new_list.append(replace)
         else:
             new_list.append(item)
-    return new_list  #[replace if item == search else item for item in my_list]
+    return new_list   #[replace if item == search else item for item in my_list]
