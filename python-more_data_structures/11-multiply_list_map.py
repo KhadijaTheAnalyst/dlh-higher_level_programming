7561cb7943466d434acb402de5eacbb0b84b8c63
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-29 17:55:26 +0200

    convert a list into a new list using map

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_map_list(my_list=[], number=0):
-    """Return a list with all values multiplied by a number."""
-    # we can use list comprehension but we will use map function instead.
-    # new_list = [x * number for x in my_list]  # --- IGNORE ---
-    new_list = list(map(lambda x: x * number, my_list))
-    return new_list
+    return list(map(lambda x: x * number, my_list))
