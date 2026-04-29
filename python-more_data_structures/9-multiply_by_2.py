fd8db36ccb324402b8be2944bb67e73e6935cf84
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-29 16:03:39 +0200

    multiplyeach value by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,10 +3,9 @@ def multiply_by_2(a_dictionary):
     # Return a new dictionary with all values multiplied by 2.
     """
     we can use dict comprehension but we will use a for loop instead.
-    new_dict = 
-    {key: value * 2 for key, value in a_dictionary.items()}
+    new_dict={key: value * 2 for key, value in a_dictionary.items()}
     for each key, value → create key: value*2 in new_dict
-    --- IGNORE --- 
+    --- IGNORE ---
     """
     new_dict = {}
     for key, value in a_dictionary.items():
