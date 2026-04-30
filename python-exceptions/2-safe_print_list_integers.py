a303074ea1c60191c14ee43c137c39397e9fff49
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-30 11:57:45 +0200

    print only integer and skip other data types

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     # Print the first x elements of a list and only integers.
-    count = 0 # number of integers printed
+    # number of integers printed
+    count = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,12 @@ def best_score(a_dictionary):
     """Return a key with the biggest integer value."""
     if not a_dictionary:
         return None
-    best_key = max(a_dictionary, key=a_dictionary.get)
+    """"we can use max function with key argument but we will use a for loop instead."""
+    # best_key = max(a_dictionary, key=a_dictionary.get)
+    best_key = None
+    best_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > best_value:
+            best_value = value
+            best_key = key
     return best_key
