a302a7613e5c45eff1e77bb8b0243217631e7b43
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-04-30 11:20:15 +0200

    print only integer and skip other data types

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     # Print the first x elements of a list and only integers.
-    """
-    i = 0
-    count = 0
-    while i < x:
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
-        i += 1
-    print()
-    return count
-    """
     count = 0 # number of integers printed
     for i in range(x):
         try:
