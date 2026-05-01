09133e8d93526cf7c2182e1d018d39b5765ad1d0
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-01 16:55:39 +0200

    printing safe error

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value: The value to be printed
+
+    Returns:
+        If value has been correctly printed - True.
+        Otherwise - False.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
