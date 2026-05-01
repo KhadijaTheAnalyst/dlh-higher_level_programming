4b1e2ab0891705fc364263819520c038e6340c09
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-01 17:08:29 +0200

    fixing errors

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     """
     Executes a function safely.
@@ -14,5 +17,5 @@ def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
         return None
