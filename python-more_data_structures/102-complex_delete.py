4f506d5adb8e2a9c5bd5d44d9b33b90d7016d207
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-03 16:37:15 +0200

    fixing errors

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -7,6 +7,8 @@ def complex_delete(a_dictionary, value):
             new_dict[key] = val
     return new_dict
 """
+
+
 def complex_delete(a_dictionary, value):
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
