3ecb6f9619606073c8a7bd6c1ac4a8111d9df5b5
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-05 23:01:30 +0200

    defining the rectangle with getter and setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Defines a rectangle."""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """Initialize the rectangle with width and height."""
         self.width = width
         self.height = height
