b15925754ddd11a84fb6adb5aff8a326947811d8
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-07 10:35:27 +0200

    converting to json from python, sets are not supported in json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module containing the to_json_string function."""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to convert to JSON string.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
