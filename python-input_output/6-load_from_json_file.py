9eafea4acff4fbfbe155120c134b10806121ba17
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-07 12:18:13 +0200

    reading python file to jason

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module containing the load_from_json_file function."""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create an Object from a "JSON file".
+
+    Args:
+        filename (str): The name of the JSON file to read from.
+
+    Returns:
+        The Python object represented by the JSON file.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        # Read the JSON string from the file and deserialize it to a Python object
+        return json.loads(f.read())
